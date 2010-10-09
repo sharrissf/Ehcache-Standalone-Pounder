802540914263ebbbb79df2dc6aa1d5adee25231e
Steve Harris <sharrissf@gmail.com>
2010-10-09 12:03:14 -0700

    fix mistake in run-pounder

--- src/assemble/run-pounder.sh
+++ src/assemble/run-pounder.sh
@@ -1,1 +1,1 @@
-java -verbose:gc  -Xms200m -Xmx200m -XX:+UseCompressedOops -XX:MaxDirectMemorySize=64G -cp ".:./jyaml-1.3.jar:./ehcache-pounder-0.0.1-SNAPSHOT.jar:./ehcache-core-ee-2.3-BETA.jar:slf4j-api-1.5.11.jar:slf4j-jdk14-1.5.11.jar" org.sharrissf.ehcache.tools.EhcachePounder
+java -verbose:gc  -Xms200m -Xmx200m -XX:+UseCompressedOops -XX:MaxDirectMemorySize=64G -cp ".:./jyaml-1.3.jar:./ehcache-pounder-0.0.2-SNAPSHOT.jar:./ehcache-core-ee-2.3-BETA.jar:slf4j-api-1.5.11.jar:slf4j-jdk14-1.5.11.jar" org.sharrissf.ehcache.tools.EhcachePounder
